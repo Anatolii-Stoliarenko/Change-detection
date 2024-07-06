db8d0c28586c25c65bdc1b7e8ffefedc23bdc4b9
Anatolii Stoliarenko <anatolii.stoliarenko@gmail.com>
2024-07-06 12:13:59 +0200

    start project

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { CounterComponent } from './counter/counter.component';
+import { MessagesComponent } from './messages/messages.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CounterComponent, MessagesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  get debugOutput() {
+    console.log('[AppComponent] "debugOutput" binding re-evaluated.');
+    return 'AppComponent Component Debug Output';
+  }
+}
